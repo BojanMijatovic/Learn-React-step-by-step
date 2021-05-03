8cffbf514e7d2a69d8f7fd70fa1091af2f397e67
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-03 21:07:41 +0200

    Start campaign 50 projects

--- Basic/Working With Components/my-first-react-app/src/App.js
+++ Basic/Working With Components/my-first-react-app/src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Features from './components/Features';
 
-function App() {
-    return ( <div className = "App" >
-        <p> Here is new </p> 
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Features />
+    </div>
+  );
+};
 
 export default App;
+
+/*
+Think of a product name
+Gather free pictures
+Create a homepage
+Create feature pages
+Create a landing page
+Add a sign-up form
+Create a contact page
+Add a contact form
+Add contact links to site
+
+
+-   Level 1
+Having a website is a great way to showcase a product with the
+ability to provide as many details as possible.
+Create a new website that highlights the great features of the
+product and provides in-depth information about each
+feature. The pages should include:
+• Homepage with photograph and high level overview
+• Feature pages linked from homepage with more details
+
+-   Level 2
+Just because you made a website, doesn’t automatically mean
+people will purchase the product.
+Create a landing page as part of the marketing website that
+drives people to sign up with a form for more information.
+
+-   Level 3
+Ultimately people need to purchase or sign up for the product
+and to do that, they need a way to contact the sales team.
+Create a new page with a form that will allow people to
+contact the company if interested in the product. Include a
+“Call to Action” link at the bottom of every page on the website
+except for the contact page itself that links to the contact page.
+
+*/
---
+++ Basic/Working With Components/my-first-react-app/src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>REACT PEOPLE Company</h1>
+      <h2>Who We are?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum minima, a
+        sapiente voluptatibus eveniet nobis laboriosam veniam deleniti eaque sed
+        quibusdam suscipit commodi placeat nihil cumque aspernatur et quo amet!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
