318f9afed5565fcfcadaf251676ff99a6430e439
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-03 13:05:40 +0200

    Writing More Complex JSX Code

--- Basic/Working With Components/01-starting-setup/src/App.js
+++ Basic/Working With Components/01-starting-setup/src/App.js
@@ -1,10 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
     return (
         <div>
-            <h2 > Let 's get started!</h2>
-            <p> This is new P tag </p>
+            <h2>Let 's get started!</h2>
+            <ExpenseItem/>
         </div>
     );
+    // Here is my first Component
 }
 
 export default App;
---
+++ Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+// A Component in React is just a simple function !
+
+// Writing More Complex JSX Code
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>March 30 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$250</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
