e43fd7589cc843c8eebecc3872d5c8b50c5c9406
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-03 16:39:27 +0200

    Passing Data via "props"

--- Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
+++ Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
@@ -4,17 +4,14 @@
 // Adding Basic CSS Styling
 import '../style/components/expenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 30);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 250;
-
+const ExpenseItem = (props) => {
+  // Passing Data via "props"
   return (
     <div className="expense-item">
-      <div>March 30 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
