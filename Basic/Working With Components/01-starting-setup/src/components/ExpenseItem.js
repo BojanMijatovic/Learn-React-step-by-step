eaac378db0e253d56981486f519182004c6a2beb
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-03 20:25:03 +0200

    practice with components

--- Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
+++ Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 // A Component in React is just a simple function !
-
+import ExpenseDate from './ExpenseDate';
 //  Import css file
 // Adding Basic CSS Styling
 import '../style/components/expenseItem.css';
 
 const ExpenseItem = (props) => {
   // Passing Data via "props"
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
