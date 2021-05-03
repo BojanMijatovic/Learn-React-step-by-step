1466614c5744c939f4515d07246373aaaf9c82a8
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-03 15:16:46 +0200

    Outputting Dynamic Data & Working with Expressions in JSX

--- Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
+++ Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,24 @@
 // A Component in React is just a simple function !
 
-// Writing More Complex JSX Code
+//  Import css file
+// Adding Basic CSS Styling
+import '../style/components/expenseItem.css';
+
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 30);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 250;
+
   return (
-    <div>
+    <div className="expense-item">
       <div>March 30 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$250</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
 };
 
+// Writing More Complex JSX Code
 export default ExpenseItem;
