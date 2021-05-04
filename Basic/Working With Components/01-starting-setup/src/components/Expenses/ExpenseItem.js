c635b219f688a98a8f274ed068fb0f9df0d82f1a
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-04 11:07:35 +0200

    Start with hook setState

--- Basic/Working With Components/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ Basic/Working With Components/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 // A Component in React is just a simple function !
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
@@ -9,13 +10,25 @@ import '../../style/components/expenseItem.css';
 const ExpenseItem = (props) => {
   // Passing Data via "props"
 
+  //  React HOOKS!!
+  // titleToManage is pointer to props.title (value how is managed)
+  //                                                                  props.title is initial value
+  const [titleToManage, setTitle] = useState(props.title);
+
+  //  here create function
+  //   name convention is to click function we add Handler on end of name
+  const clickHandler = () => {
+    setTitle('Here is new title');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{titleToManage}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
