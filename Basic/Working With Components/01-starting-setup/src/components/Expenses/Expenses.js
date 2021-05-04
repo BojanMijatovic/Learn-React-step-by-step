c3b4a2755baa44abf92928ffd1ea8a776e2e0f73
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-04 16:56:40 +0200

    example

--- Basic/Working With Components/01-starting-setup/src/components/Expenses/Expenses.js
+++ Basic/Working With Components/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,40 +1,44 @@
 import '../../style/components/expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
-// Assignment: Time to Practice: React & Component Basics
-/*Complete the instructions mentioned in the above video.
-In detail:
-    Create a new component that is responsible for displaying expenses
-    Add multiple ExpenseItem components in that component
-    Keep the expenses data in the App component and pass that data into the newly created component
-You can use this .css file for some styling for your Expenses 
-*/
+import { useState } from 'react';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('');
+
+  const addFilterByYear = (newYear) => {
+    setFilteredYear(newYear);
+    console.log(`Here is from Expenses ${newYear}`);
+  };
+
   return (
-    <Card className='expenses'>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter showYearHandler={addFilterByYear} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 
