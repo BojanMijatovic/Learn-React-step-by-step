413a1bf727470ec1c2060713b926be1e6f55b82c
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-07 19:10:55 +0200

    againg with props

--- Basic/Working With Components/work-with-jsx/src/App.js
+++ Basic/Working With Components/work-with-jsx/src/App.js
@@ -1,5 +1,16 @@
 const App = () => {
-  return <p>Here is P tag</p>;
+  const btnText = 'Click me';
+  return (
+    <div>
+      <label htmlFor='name' className='label'>
+        Name
+      </label>
+      <input type='text' placeholder='input name' />
+      <button type='submit' style={{ backgroundColor: 'red', color: 'white' }}>
+        {btnText}
+      </button>
+    </div>
+  );
 };
 
 export default App;
