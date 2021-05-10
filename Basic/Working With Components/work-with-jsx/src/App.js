20ea5dc360fecdd982eaf74b76d47076e9133e6e
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-10 12:01:06 +0200

    Component Reuse

--- Basic/Working With Components/work-with-jsx/src/App.js
+++ Basic/Working With Components/work-with-jsx/src/App.js
@@ -1,11 +1,35 @@
-import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
-const App = (props) => {
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail
+          author='Nicolas'
+          timeAgo='Today at 16:45 pm'
+          text='Great tutorial thanks'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Ted'
+          timeAgo='Today at 18:05 pm'
+          text='You are welcome'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Mike'
+          timeAgo='Yesterday at 06:45 am'
+          text='This is awesome'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
