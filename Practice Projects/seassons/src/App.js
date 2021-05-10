abf097c5c9f944cfbcb378c9dff53d5a50aa0ce8
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-10 16:56:59 +0200

    Conditionally Rendering Content

--- Practice Projects/seassons/src/App.js
+++ Practice Projects/seassons/src/App.js
@@ -1,17 +1,45 @@
-import SeasonDisplay from './SeasonDisplay';
+import React from 'react';
 
-function App() {
-  const test = window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position.coords),
-    (error) => console.log(error.message)
-  );
+// function App() {
+//   const test = window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position.coords),
+//     (error) => console.log(error.message)
+//   );
 
-  return (
-    <div>
-      <h2>Latitude</h2>
-      <SeasonDisplay />
-    </div>
-  );
+//   return (
+//     <div>
+//       <h2>Latitude</h2>
+//       <SeasonDisplay />
+//     </div>
+//   );
+// }
+
+/*
+ The Rules of State
+*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      errorMessage: '',
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => this.setState({ errorMessage: error.message })
+    );
+  }
+  render() {
+    return (
+      <div>
+        Latitude: {`${this.state.latitude || this.state.errorMessage} `}
+      </div>
+    );
+  }
 }
 
 export default App;
