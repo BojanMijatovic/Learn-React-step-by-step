ea0861c98dcf05dabe871108d141e1c9c67acb1a
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-11 12:56:50 +0200

    practice with states

--- Basic/Working With Components/practice-state/src/App.js
+++ Basic/Working With Components/practice-state/src/App.js
@@ -1,13 +1,26 @@
-import ListCar from './components/ListCar';
+import React from 'react';
+import Car from './components/Car';
 
-import styles from './components/App.module.css';
+class App extends React.Component {
+  state = {
+    name: 'audi',
+    model: 'A8',
+  };
 
-const App = () => {
-  return (
-    <div className={styles.app}>
-      <ListCar />
-    </div>
-  );
-};
+  componentDidMount() {
+    this.setState({ name: 'new car' });
+    this.setState({ model: 'A6' });
+  }
+
+  render() {
+    return (
+      <div>
+        <Car carName={this.state.name} carModel={this.state.model} />
+        <Car carModel={this.state.model} />
+        <Car />
+      </div>
+    );
+  }
+}
 
 export default App;
--- Basic/Working With Components/practice-state/src/components/Car.js
+++ Basic/Working With Components/practice-state/src/components/Car.js
@@ -1,13 +1,15 @@
-import styles from './Car.module.css';
-
 const Car = (props) => {
   return (
-    <div className={styles.car}>
-      <h3>Name: {props.name}</h3>
-      <h4>Mark: {props.mark}</h4>
-      <h4>Model: {props.model}</h4>
+    <div>
+      <h4>Name {props.carName}</h4>
+      <p>Model {props.carModel}</p>
     </div>
   );
+};
+
+Car.defaultProps = {
+  carName: 'Insert car',
+  carModel: 'Add model please',
 };
 
 export default Car;
