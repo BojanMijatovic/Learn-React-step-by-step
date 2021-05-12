2b22b1d076d19038cee66cd053748825f85ccca4
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-12 12:13:21 +0200

    add axios

--- Basic/Working With Components/user-inputs-and-events/src/components/App.js
+++ Basic/Working With Components/user-inputs-and-events/src/components/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className='ui container' style={{ marginTop: '10px' }}>
-      <h1>Welcome to new app</h1>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos/', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: 'Client-ID t6HDttpyxSWhm0BZIeFbjXqQ4J3XKEWoF9J0iVUObV8',
+      },
+    });
+  }
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        <h1>Welcome to new app</h1>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
