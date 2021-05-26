0343a58ee5f951a7c4640b2bbad8bc18a2b37030
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-26 12:44:59 +0200

    end of translate app

--- Basic/Hooks/start-with-hooks/src/components/Convert.js
+++ Basic/Hooks/start-with-hooks/src/components/Convert.js
@@ -1,11 +1,70 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  // useEffect(() => {
+  //   const doTranslation = async () => {
+  //     const { data } = await axios.post(
+  //       'https://translation.googleapis.com/language/translate/v2',
+  //       {},
+  //       {
+  //         params: {
+  //           q: text,
+  //           target: language.value,
+  //           key: API_KEY,
+  //         },
+  //       }
+  //     );
+  //     setTranslated(data.data.translations[0].translatedText);
+  //   };
+
+  //   const setTimeoutTranslate = setTimeout(() => {
+  //     if (text) {
+  //       doTranslation();
+  //     }
+  //   }, 500);
+  //   return () => {
+  //     clearTimeout(setTimeoutTranslate);
+  //   };
+  // }, [language, text]);
+
   useEffect(() => {
-    console.log(`New text`);
-    return () => {};
-  }, [language, text]);
-  return <div></div>;
+    const timer = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: API_KEY,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
+    <div>
+      <h1 className='ui header'>{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
