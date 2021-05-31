4fb9fccb42e689c13d29f62f39262e2acb62d35e
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-31 20:01:05 +0200

    start weather app

---
+++ Practice Projects/weather/src/components/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <h1>Start new app</h1>
+    </div>
+  );
+};
+
+export default App;
