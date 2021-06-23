cfa3777e38fed6510a05df69162125dcc43e1056
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-06-23 15:22:13 +0200

    First Summary

--- Basic/Working With Components/01-starting-setup/src/App.js
+++ Basic/Working With Components/01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 const App = () => {
   const expenses = [
@@ -26,10 +26,7 @@ const App = () => {
   return (
     <div>
       <h1>Here is second time in React</h1>
-      <p>This also works!</p>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
--- Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
+++ Basic/Working With Components/01-starting-setup/src/components/ExpenseItem.js
@@ -1,20 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
 
-  //  date
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const year = date.getFullYear();
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
-
   return (
     <div className='expense-item'>
-      <div>
-        <div className=''>{month}</div>
-        <div className=''>{year}</div>
-        <div className=''>{day}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
