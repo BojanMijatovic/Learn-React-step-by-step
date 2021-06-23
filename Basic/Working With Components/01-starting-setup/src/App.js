9f4c25dd795b2692341a365f48fa14f1528534a3
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-06-23 11:07:35 +0200

    back to react

--- Basic/Working With Components/01-starting-setup/src/App.js
+++ Basic/Working With Components/01-starting-setup/src/App.js
@@ -1,24 +1,5 @@
-import { useState } from 'react';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
-
-const expensesTest = [];
-
 const App = () => {
-  const [expenses, setExpenses] = useState(expensesTest);
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
-
-  return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
-    </div>
-  );
+  return <h1>Here is second time in React</h1>;
 };
 
 export default App;
