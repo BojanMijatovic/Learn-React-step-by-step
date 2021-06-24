8c90674d558c0f4d7a91701f581c2440dbe4d77d
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-06-24 12:05:24 +0200

    learn useState Hook

--- Basic/Working With Components/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ Basic/Working With Components/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
+  const [newTitle, setNewTitle] = useState(title);
+
+  const onButtonClick = () => {
+    setNewTitle('New Title');
+    console.log(`Test`);
+  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className='expense-item__price'>${amount}</div>
       </div>
+      <button onClick={onButtonClick}>change title</button>
     </Card>
   );
 };
