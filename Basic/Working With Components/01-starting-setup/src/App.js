e6e797c5ce1a28f860a04e7d2f5f0ee5faeee1cc
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-06-25 13:20:08 +0200

    lift up from child to parent example

--- Basic/Working With Components/01-starting-setup/src/App.js
+++ Basic/Working With Components/01-starting-setup/src/App.js
@@ -1,6 +1,9 @@
+import React, { useState } from 'react';
+
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
+  const [expensesData, setExpensesData] = useState([]);
   const expenses = [
     {
       title: 'Car Insurance',
@@ -10,7 +13,7 @@ const App = () => {
     {
       title: 'House rent',
       amount: 500,
-      date: new Date(2021, 5, 1),
+      date: new Date(2020, 5, 1),
     },
     {
       title: 'Beer',
@@ -23,10 +26,18 @@ const App = () => {
       date: new Date(2021, 2, 8),
     },
   ];
+
+  const addExpense = (expense) => {
+    setExpensesData((addNewExpenses) => {
+      return [...addNewExpenses, expense];
+    });
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpense} />
       <Expenses expenses={expenses} />
+      {console.log({ expensesData })}
     </div>
   );
 };
--- Basic/Working With Components/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ Basic/Working With Components/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,13 +1,13 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = ({ onAddExpense }) => {
   const saveExpenseData = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    onAddExpense(expenseData);
   };
   return (
     <div className='new-expense'>
